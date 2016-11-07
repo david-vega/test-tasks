0960079f4006f85e4a86211ef761ef9fcce63952
David Vega <zetacu@gmail.com>
2016-11-06 21:05:36 -0800

    mark as read in small chuncks

--- messaging/app/models/sms_message.rb
+++ messaging/app/models/sms_message.rb
@@ -1,4 +1,5 @@
 class SmsMessage < ActiveRecord::Base
+  BATCH_SIZE = 20
   belongs_to :account
   validates :account, presence: true
 
@@ -9,11 +10,19 @@ class SmsMessage < ActiveRecord::Base
   scope :unread, -> { where(unread: true) }
   scope :outbound, -> { where(outbound: true) }
   scope :sorted, -> { order(created_at: :desc) }
-  scope :thread, -> (*numbers){ where(from_number: numbers, to_number: numbers) }
+  scope :thread_by_numbers, -> (*numbers){ where(from_number: numbers, to_number: numbers) }
+  scope :thread, -> (thread_uuid){ where(thread_uuid: thread_uuid) }
+
+  def self.mark_thread_as_read!(thread_uuid:)
+    SmsMessage.thread(thread_uuid).unread.find_in_batches(batch_size: BATCH_SIZE) do |sms_messages|
+      sms_messages.update_all(unread: false)
+    end
+  end
 
   private
 
   def set_thread_uuid
-    self.thread_uuid = SmsMessage.thread(from_number, to_number).last.try(:thread_uuid) || SecureRandom.uuid
+    self.thread_uuid = SmsMessage.thread_by_numbers(from_number, to_number).last.try(:thread_uuid) ||
+                       SecureRandom.uuid
   end
 end
